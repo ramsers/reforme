af086d210c4739f757d74db18078dc33354ec7a6
Raheim Bailey <raheimbbailey@gmail.com>
2025-10-09 11:53:15 -0400

    begin checkout sessions with command

--- apps/payment/commandBus/command_handlers.py
+++ apps/payment/commandBus/command_handlers.py
@@ -1,4 +1,26 @@
 from apps.payment.commandBus.commands import CreatePassPurchaseCommand
 
 def handle_create_pass_purchase(command: CreatePassPurchaseCommand):
-    print('Im hitting purchase command ===========')
+    user = User.objects.get(id=command.user_id)
+    print('Im hitting purchase command ===========', user)
+
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            customer_email=user.email,
+            payment_method_types=["card"],
+            line_items=[{
+                "price": command.price_id,
+                "quantity": 1,
+            }],
+            mode="subscription" if command.is_subscription else "payment",
+            # success_url=settings.FRONTEND_URL + "/success",
+            # cancel_url=settings.FRONTEND_URL + "/cancel",
+            metadata={
+                "user_id": user.id,
+                "product_name": command.product_name,
+            },
+        )
+    except Exception as e:
+        print('MOLO GOLO SHOLO', e)
+
+    return checkout_session.id
