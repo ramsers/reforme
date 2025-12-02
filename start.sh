e570d7fb0126a96e6bf03b239b1271a1ab3d87b4
Raheim Bailey <raheimbbailey@gmail.com>
2025-12-02 10:42:04 -0500

    clean up

--- start.sh
+++ start.sh
@@ -8,5 +8,7 @@ echo "Starting RQ worker in background..."
 python manage.py rqworker default &
 
 echo "Starting Gunicorn web server..."
-gunicorn reformeApi.wsgi:application --bind 0.0.0.0:$PORT --timeout 120
-chmod +x start.sh
+exec gunicorn reformeApi.wsgi:application \
+    --bind 0.0.0.0:$PORT \
+    --timeout 120 \
+    --workers 3
