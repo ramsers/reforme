33c42462ad1a9697cc0d326eb1e107af6f9f0508
Raheim Bailey <49011638+ramsers@users.noreply.github.com>
2025-12-07 13:22:54 -0500

    Use environment default timezone in class filters

--- apps/classes/filter/classes_filter.py
+++ apps/classes/filter/classes_filter.py
@@ -1,12 +1,16 @@
+import os
+from django.conf import settings
 import django_filters
 from apps.classes.models import Classes
-from django.db.models import Count, Q
+from django.db.models import Count, Q, F, Value
+from django.db.models.functions import Coalesce, TruncDate
+from django.db.models import Func
 
 
 class ClassesFilter(django_filters.FilterSet):
-    date = django_filters.DateFilter(field_name="date", lookup_expr="date")
-    start_date = django_filters.DateTimeFilter(field_name="date__date", lookup_expr="gte")
-    end_date = django_filters.DateTimeFilter(field_name="date__date", lookup_expr="lte")
+    date = django_filters.DateFilter(method="filter_by_local_date")
+    start_date = django_filters.DateFilter(method="filter_by_start_date")
+    end_date = django_filters.DateFilter(method="filter_by_end_date")
     title = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
     has_bookings = django_filters.BooleanFilter(method="filter_has_bookings")
     search = django_filters.CharFilter(method="filter_search")
@@ -24,6 +28,30 @@ class ClassesFilter(django_filters.FilterSet):
         return queryset.filter(
             Q(title__icontains=value) | Q(description__icontains=value) | Q(instructor__name__icontains=value)
         )
+
+    def _with_local_date(self, queryset):
+        default_timezone = os.environ.get("TIME_ZONE", settings.TIME_ZONE)
+        timezone_expr = Coalesce(F("instructor__account__timezone"), Value(default_timezone))
+        local_time_expr = Func(timezone_expr, F("date"), function="timezone")
+        return queryset.annotate(local_date=TruncDate(local_time_expr))
+
+    def filter_by_local_date(self, queryset, name, value):
+        if not value:
+            return queryset
+        queryset = self._with_local_date(queryset)
+        return queryset.filter(local_date=value)
+
+    def filter_by_start_date(self, queryset, name, value):
+        if not value:
+            return queryset
+        queryset = self._with_local_date(queryset)
+        return queryset.filter(local_date__gte=value)
+
+    def filter_by_end_date(self, queryset, name, value):
+        if not value:
+            return queryset
+        queryset = self._with_local_date(queryset)
+        return queryset.filter(local_date__lte=value)
 
     @property
     def qs(self):
